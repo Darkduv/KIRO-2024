fdba10b6c7f246b2f18b919fdd46ae08eaf80a8a
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-11-09 10:50:12 +0100

    exemples d'utilisation de graphes

---
+++ KIRO/structures/graph.py
@@ -0,0 +1,70 @@
+####### ON UTILISE NETWORKX 
+# LE RESTE EST JUSTE UN EXEMPLE DE L'UTILISATION DE NX
+
+# Allez voir ça !! 
+# https://networkx.org/documentation/stable/tutorial.html
+
+# imports 
+import networkx as nx
+
+# On crée un graphe 
+G = nx.Graph()
+DirectedG = nx.DiGraph()
+MultipleEdgesG = nx.MultiGraph()
+
+# On ajoute des noeuds, avec des attributs si besoin
+for i in range(10):
+    G.add_nodes_from(
+        [
+            (
+                i,
+                {"color":['green', 'red', 'blue'][i%3]}
+            ),
+            (
+                i+100,
+                {"color":['green', 'red', 'blue'][i%3]}
+            ),
+        ]
+    )
+
+# On ajoute des edges, avec des attributs aussi 
+from random import choice, random
+
+for _ in range(12):
+    
+    G.add_edges_from(
+        [
+            (
+                choice(list(G._node)),
+                choice(list(G._node)),
+                {"weight" : random()}
+            )
+        ]
+    )
+
+# --- On peut trouver la data comme ceci
+
+#  Renvoie None si pas d'Edge
+G.get_edge_data(u=1, v=2)
+
+# Renvoie tous les noeuds avec leurs attributs (dictionnaire)
+G._node
+
+# Renvoie un noeud en particulier (pour _node, clé = le noeud, valeur = attributs)
+G._node[102]
+
+# Calcule les noeuds adjacents d'un noeud (avec les attributs des edges)
+# adj a pour clés les noeuds, valeurs des dictionnaires avec clés noeuds et 
+#   et valeurs = attributs des edges
+G.adj[102]
+G[102] # Syntaxe différente
+
+# --- On peut plot le graph aussi 
+import matplotlib.pyplot as plt
+
+subax1 = plt.subplot(121)
+nx.draw(G, with_labels=True, font_weight='bold')
+subax1 = plt.subplot(122)
+nx.draw_shell(G, with_labels=True, font_weight='bold')
+
+plt.show()
--- _MEE.py
+++ _MEE.py
@@ -16,37 +16,3 @@ class Template2 :
 # to_json(
 #     {1:1, 2:'a', 3:Template(1,2,3), 4:[Template(1,2,3), Template2(1,2,3)]}
 # )
-
-import random
-
-from KIRO.algorithms import GeneticAlgorithm
-
-
-population_size = 500
-gene_length = 10
-compute_generations = 1000
-mutation_rate = 0.05
-
-initial_population = [
-    [random.random() for _ in range(gene_length)]
-    for _ in range(population_size)
-]
-
-class GeneticTest(GeneticAlgorithm):
-    def fitness(self, individual: Any) -> float:
-        return -sum([elem**2 for elem in individual])
-    
-    def crossover(self, indiv1: Any, indiv2: Any) -> Any:
-        return indiv1[:gene_length//2]+indiv2[gene_length//2:]
-    
-    def mutate(self, indiv1: Any) -> Any:
-        return [elem+(random.random()-0.5)*mutation_rate for elem in indiv1]
-
-ALGO = GeneticTest(
-    initial_population=initial_population,
-    number_of_best_indiv=population_size//10,
-)
-
-ALGO.compute_generations(compute_generations)
-
-print(ALGO.last_generation)
