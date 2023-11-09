e28073b24f91ce8819209c90877f07f8162a4ca5
Jean-Sébastien Verhaeghe <jeansebastien.verhaeghe@gmail.com>
2023-11-09 18:02:08 +0100

    Fix by_id

--- KIRO/structures/parameters.py
+++ KIRO/structures/parameters.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from KIRO.utils import ById
 
 @dataclass
-class Parameters(ById):
+class Parameters:
     fixed_cost_cable        : float
     variable_cost_cable     : float
     curtailing_penalty      : float
