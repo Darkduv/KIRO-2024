2ecc10a4fc345fa982967fad3376b2a49b401119
Darkduv <40823426+Darkduv@users.noreply.github.com>
2023-11-09 15:33:34 +0100

    nettoyage ancien code et préparation pour les vues

--- moulinette/view/default.py
+++ moulinette/view/default.py
@@ -5,5 +5,6 @@ from model import *
 
 def view(instance: Instance):
     """Do nothing."""
+    ## TODO TODO vue par défaut
     print(instance.parameters.size)
     print(instance.parameters.costs)
